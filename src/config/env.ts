5b66ba9227614bc5557d63733f6eca9636d512f9
teckmk <skamky4@gmail.com>
2025-10-09 08:31:15 +0500

    chore: remove react/vite leftover files

--- src/config/env.ts
+++
@@ -1,9 +0,0 @@
-interface EnvConfig {
-  googleFormUrl: string;
-}
-
-const env: EnvConfig = {
-  googleFormUrl: import.meta.env.VITE_GOOGLE_FORM_URL || 'https://forms.google.com/your-form-id',
-};
-
-export default env;
